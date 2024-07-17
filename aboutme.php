2550b1b961e1e83fa929ba2edd95f577c17388c1
AchmadMahfud26 <achmadmahfud01@gmail.com>
2024-07-17 15:17:14 +0700

    update1

--- aboutme.php
+++ aboutme.php
@@ -486,11 +486,6 @@
             </a>
         </div>
 
-
-        <!-- Back to Top -->
-        <a href="#" class="btn btn-secondary py-3 fs-4 back-to-top"><i class="bi bi-arrow-up"></i></a>
-
-
         <!-- JavaScript Libraries -->
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
